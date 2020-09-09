f712c9a7b6a42b5d1cc1eef9b0662f82cdc072c1
Amanda Heideman <heideman.amanda@gmail.com>
2020-09-09 10:39:02 +0200

    experimenting with js

--- js/script1.js
+++ js/script1.js
@@ -1,3 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", function(e){
+    let fav_color=document.getElementById('color').value;
+    let save_button=document.getElementById("save");
+    save_button.addEventListener("click", function(event){
+        console.log(click);
+        document.body.style.backgroundColor = "fav_color".value;
+    });
 });
